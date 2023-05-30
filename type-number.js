062d3e9b37802300331591bf925886c33479dfec
ArthurSPereira3156 <53497812+ArthurSPereira3156@users.noreply.github.com>
2023-05-29 22:11:53 -0300

    Termino das variaveis tipo number

---
+++ type-number.js
@@ -0,0 +1,21 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const soma = primeiroNumero - segundoNumero;
+
+console.log(soma)
+
+// pontos flutuantes
+
+const numeropontoFlutuante = 3.3;
+
+const novaoperacao = primeiroNumero / numeropontoFlutuante;
+
+console.log(novaoperacao);
+
+// NaN -> Not a Number (nao é um número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero)
