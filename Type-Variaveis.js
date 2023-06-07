fb14985e4b5db9e6971b85fccd8e630db70f82bd
ArthurSPereira3156 <53497812+ArthurSPereira3156@users.noreply.github.com>
2023-06-07 17:11:11 -0300

    término das variaveis  e inicio do Truthy-falsy

--- Type-Variaveis.js
+++ Type-Variaveis.js
@@ -1,7 +1,38 @@
 //VAR
+//COST = VALOR UNICO QUE NAO PODE SER ALTERADO
+//LET = VARIAVEIS QUE PODEM TER OS VALORES MUDADOS
+//var altura = 5;
+//var comprimento =7;
+//area = altura * comprimento;
+//console.log(area);
+//var area
+//let forma = 'retângulo'
+//let altura = 5;
+//let comprimento = 7;
+//let area;
+//if (forma == 'retângulo')
+//{
+//    area = altura * comprimento;
+//}
+//lse
+//{
+//    area = (altura * comprimento) / 2;
+//}
+//console.log(area);
 
-var altura = 5;
-var comprimento =7;
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
 
-area = altura * comprimento;
-console.log(area);
+if (forma === 'quadrado')
+{
+    area = altura * comprimento;
+}
+else
+{
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
+   
