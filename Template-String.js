147035f5a822ae4045f496a11bdc8207ba8728d9
ArthurSPereira3156 <53497812+ArthurSPereira3156@users.noreply.github.com>
2023-06-16 18:41:44 -0300

    Template-String,funcoes,parametros

---
+++ Template-String.js
@@ -0,0 +1,7 @@
+const nome = "Ju";
+const idade = 2021-1981;
+const CidadeDeNascimento = "São Paulo";
+
+//const apresentacao = "Meu nome é " + nome + ", minha idade é " +idade + " e nasci na cidade de " + CidadeDeNascimento;
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${CidadeDeNascimento}`
+console.log(apresentacao)
---
+++ funcoes.js
@@ -0,0 +1,14 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1) declara a função
+
+function imprimeTexto(texto)
+{
+    console.log(texto)
+}
+
+// 2) executa a função (1 ou + vezes)
+
+imprimeTexto("oi");
