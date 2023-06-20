ad8a0b0dd168c086e48c09cb14e24ecb6c1244ee
ArthurSPereira3156 <53497812+ArthurSPereira3156@users.noreply.github.com>
2023-06-20 20:05:32 -0300

    continuaçao-arrays

--- Arrays.js
+++ Arrays.js
@@ -7,6 +7,30 @@
 //               0    1  2   3
 const notas =  [10, 6.5, 8, 7.5];
 
-const media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(media);
+
+const numero1 = 50;
+const numero2 = 43;
+const numero3 = 12;
+
+// utilizando array
+//const numeros = [50, 43, 12];
+
+const arrayDeNumeros = [50, 43, 12];
+const arrayDeStrings = ["banana", "alura", "Juliana"];
+
+// array com 1 elemento
+const numero = [5];
+
+// array com 3 elementos
+//const numeros = [50, 43, 12];
+
+//const numeros = [50, 43, 12];
+// o número 43 está na posição de número 1 (segunda posição) nessa lista de três números.
+// índice     0   1   2   3
+//const numeros = [50, 43, 12, 98];
+// índice     0   1   2   3
+const nums = [50, 43, 12, 98];
+console.log(nums.length) //4
