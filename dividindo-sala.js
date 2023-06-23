6332818c097d3e13b0d585476e32c81b439fc4aa
ArthurSPereira3156 <53497812+ArthurSPereira3156@users.noreply.github.com>
2023-06-23 17:05:04 -0300

    conclusao modulo um e dois

---
+++ dividindo-sala.js
@@ -0,0 +1,30 @@
+const alunos = [ 
+      'joao',
+      'juliana', 
+      'ana',
+      'Caio', 
+      'Lara', 
+      'marjorie', 
+      'Guilherme', 
+      'aline', 
+      'Fabiana', 
+      'Andre', 
+      'Carlos', 
+      'Paulo', 
+      'Bia', 
+      'Vivian', 
+      'Isabela', 
+      'Vinicius', 
+      'Renan', 
+      'Reanta', 
+      'Daisy', 
+      'Camilo'
+    ];
+
+    const sala1 = alunos.slice(0, alunos.length / 2);
+    const sala2 = alunos.slice(alunos.length / 2);
+
+    console.log(sala1);
+    console.log(sala2);
+    
+    
