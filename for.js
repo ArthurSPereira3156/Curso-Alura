6f0ab0328c84226dcc19a3d7a227ad49d9f9adea
ArthurSPereira3156 <53497812+ArthurSPereira3156@users.noreply.github.com>
2023-06-29 21:38:14 -0300

    inicio-do-uso-do-for

---
+++ for.js
@@ -0,0 +1,10 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+// Primeira expressão: é executada apenas uma única vez
+// Segunda expressão: condição de execução 
+// terceira expressão: é executada sempre ao final do bloco
+
+for (let indice = 0; indice <6; indice++) 
+{
+  console.log(numeros[indice]);
+}
