4f275bca20126cfa6586b2898a65d11391245a7b
ArthurSPereira3156 <53497812+ArthurSPereira3156@users.noreply.github.com>
2023-07-06 20:11:07 -0300

    termino modulo 4

---
+++ map-nomes.js
@@ -0,0 +1,8 @@
+const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva'];
+
+const nomesPadronizados = nomes.map((nomes) =>
+{
+    return nomes.toUpperCase();
+})
+
+console.log(nomesPadronizados);
